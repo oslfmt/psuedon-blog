b409d01c5e32a73e082ea96029d010746e5751b3
Victor <vw1262@gmail.com>
2024-07-05 12:09:36 -0500

    get actix to serve index.html at root URL

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpResponse, Responder, HttpServer};
+use actix_files as fs;
+
+async fn index() -> Result<fs::NamedFile, std::io::Error> {
+    // serve the index.html from dist directory
+    fs::NamedFile::open("../frontend/dist/index.html")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index)) // todo: determine if we need this, since service seems to handle loading index.html???
+            .service(fs::Files::new("/", "../frontend/dist").index_file("index.html"))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
