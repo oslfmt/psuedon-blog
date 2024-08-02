f92d17544c73be37b8d80325b156db4adf88f228
Victor <vw1262@gmail.com>
2024-08-01 21:37:00 -0500

    minor style changes to form

--- frontend/src/pages/post_create.rs
+++ frontend/src/pages/post_create.rs
@@ -16,11 +16,23 @@ pub fn post_create_form() -> Html {
                 <div class="form-div">
                     <label for="title" class="form-label">{"Title"}</label>
                     <input/>
-                    // <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"</input>
+                </div>
+                <div class="form-div">
+                    <label for="tags" class="form-label">{"Date"}</label>
+                    <input type="date"/>
                 </div>
                 <div class="form-div">
                     <label for="content" class="form-label">{"Content"}</label>
                     <textarea rows="15" cols="80" id="content" />
+                </div>
+                <div class="form-div">
+                    <label for="tags" class="form-label">{"Tags"}</label>
+                    // TODO: make this nicer, because it is not nice
+                    <select name="cars" id="cars" multiple=true>
+                        <option value="volvo">{"blockchain"}</option>
+                        <option value="saab">{"random"}</option>
+                        <option value="opel">{"philosophy"}</option>
+                    </select>
                 </div>
                 <div class="form-div">
                     <button type="submit" class="submit-btn">{"Submit"}</button>
